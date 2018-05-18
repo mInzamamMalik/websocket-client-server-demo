6da4d1e6c24ed10d66d390f180081627a736b6e2
Mohammad Inzamam Malik <malikasinger1@gmail.com>
2018-05-18 22:51:14 +0500

    reply from server

--- server.js
+++ server.js
@@ -5,12 +5,16 @@ var s = new server({ port: 3001 })
 
 // s.once
 s.on("connection", (ws) => {
-    
-    
+
+
     // any message sent from the client will receive here
     ws.on('message', actualMessage => {
         console.log("received: ", actualMessage);
+
+        if (actualMessage == "message from client") {
+            ws.send("reply from server")
+        }
     })
-    
+
 
 })
