5a92e2ebca79617d17a155381313eb24575f6931
Mohammad Inzamam Malik <malikasinger1@gmail.com>
2018-05-19 14:17:56 +0500

    final touch

--- server.js
+++ server.js
@@ -5,18 +5,23 @@ var s = new server({ port: 3001 })
 
 // s.once
 s.on("connection", (ws) => {
+    console.log("i got connected a new client");
+    // console.log("all connected clients array: ", s.clients)
+
+    // // to send message to all connected clients
+    // s.clients.map(eachClient=>{
+    //     eachClient.send("some message" + actualMessage)
+    // })
 
 
-    // any message sent from the client will receive here
+    // any message sent by the client will receive here
     ws.on('message', actualMessage => {
         console.log("received: ", actualMessage);
-
-        if (actualMessage == "message from client") {
-            ws.send("reply from server")
-        }
-
         ws.send("reply from server")
     })
 
-
+    //when a browser window close, or somehow a client is disconnect
+    ws.on("close", () => {
+        console.log("i Lost a client");
+    })
 })
